7a391572330b46020274b02415b1de7e918c6f51
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-09-06 17:07:27 +0300

    Edited app.js

--- 01.task-manager/app.js
+++ 01.task-manager/app.js
@@ -6,6 +6,13 @@ app.get('/hello', (req, res) => {
     res.send('Task manager app')
 })
 
+// app.get('/api/v1/tasks')          - get all tasks
+// app.post('/api/v1/tasks')         - create new task
+// app.get('/api/v1/tasks/:id')      - get single task
+// app.patch('/api/v1/tasks/:id')    - update task
+// app.delete('/api/v1/tasks/:id')   - delete task
+
+
 
 const port = 3000
 app.listen(port, () => {
