79f738733859c640b93123c77d999070e42fee4e
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-09-06 20:24:45 +0300

    Edited app.js

--- 01.task-manager/app.js
+++ 01.task-manager/app.js
@@ -6,18 +6,7 @@ const tasksRoutes = require('./routes/tasks.js');
 app.use(express.json())
 
 // Routes
-app.get('/hello', (req, res) => {
-    res.send('Task manager app')
-})
-
 app.use('/api/v1/tasks', tasksRoutes)
-
-// app.get('/api/v1/tasks')          - get all tasks
-// app.post('/api/v1/tasks')         - create new task
-// app.get('/api/v1/tasks/:id')      - get single task
-// app.patch('/api/v1/tasks/:id')    - update task
-// app.delete('/api/v1/tasks/:id')   - delete task
-
 
 
 const port = 3000
