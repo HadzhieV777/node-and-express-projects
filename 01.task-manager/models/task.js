9e12d52677bf4666914a1b934a52348d5a5c7756
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-09-07 20:40:28 +0300

    Edited task.js

--- 01.task-manager/models/task.js
+++ 01.task-manager/models/task.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, 'Please provide a name!'],
+    trim:true,
+    maxlength: [20, 'Name cannot contain more than 20 characters!']
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-module.exports = mongoose.model('Task', TaskSchema)
+module.exports = mongoose.model("Task", TaskSchema);
 // A model is a class with which we construct documents. In this case, each document will be a
 // kitten with properties and behaviors as declared in our schema.
 // The model is a wrapper for the schema
