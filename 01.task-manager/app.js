22af5ca637393720a37b59a147cf1c57b0d5cca1
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-09-07 09:30:23 +0300

    Edited app.js

--- 01.task-manager/app.js
+++ 01.task-manager/app.js
@@ -1,15 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const tasksRoutes = require('./routes/tasks.js');
+const tasksRoutes = require("./routes/tasks.js");
+const connectDB = require("./db/connect.js");
+require('dotenv').config()
 
 //  Middlewares
-app.use(express.json())
+app.use(express.json());
 
 // Routes
-app.use('/api/v1/tasks', tasksRoutes)
+app.use("/api/v1/tasks", tasksRoutes);
 
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    const port = 3000;
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error)
+  }
+};
 
-const port = 3000
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}...`)
-})
+start()
+
+// 01: 16: 15
