065a89b90115002ceaa7ee4cb118a03978b899a1
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-09-07 16:23:27 +0300

    Edited task.js

---
+++ 01.task-manager/models/task.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({});
+
+// A model is a class with which we construct documents. In this case, each document will be a 
+// kitten with properties and behaviors as declared in our schema.
