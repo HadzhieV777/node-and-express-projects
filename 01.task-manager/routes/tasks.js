7d767a67037a25e268e09a81a654894a3b5b4164
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-09-08 16:08:43 +0300

    Edited tasks.js

--- 01.task-manager/routes/tasks.js
+++ 01.task-manager/routes/tasks.js
@@ -8,9 +8,12 @@ const {
   deleteTask,
 } = require("../controllers/tasks.js");
 
-
 router.route("/").get(getAllTasks).post(createTask);
 
-router.route("/:id").get(getSingleTask).patch(updateTask).delete(deleteTask);
+router
+  .route("/:id")
+  .get(getSingleTask)
+  .patch(updateTask)
+  .delete(deleteTask)
 
 module.exports = router;
