fc495708e65c0690622706edce31585faee2236e
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-09-09 09:38:08 +0300

    Edited error-handler.js

---
+++ 02.store-API/middleware/error-handler.js
@@ -0,0 +1,6 @@
+const errorHandler = async (err, req, res, next) => {
+    console.log(err)
+    return res.status(500).json({msg: 'Something went wrong, please try again'})
+}
+
+module.exports = errorHandler
