5c17ab8d480d5c8e8b3f6261e12e6feeb78bdecc
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-09-10 15:35:47 +0300

    Edited app.js

--- 02.store-API/app.js
+++ 02.store-API/app.js
@@ -4,8 +4,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const productsRouter = require('./routes/products')
+
 const notFoundMiddleware = require("./middleware/not-found.js");
-const errorMiddleware = require("./middleware/error-handler.js");
+const errorHandlerMiddleware = require("./middleware/error-handler.js");
 
 // Middleware
 app.use(express.json());
@@ -16,22 +18,24 @@ app.get("/", (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/products">Products</a>');
 });
 
+app.use('/api/products', productsRouter)
+
 //  Proudcts route
 app.use(notFoundMiddleware);
-app.use(errorMiddleware);
+app.use(errorHandlerMiddleware);
 
-// const port = proccess.env.PORT || 3000;
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 const start = async () => {
   try {
     // connect DB
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
     console.log(error);
   }
 };
 
-// 03: 18: 01
+//  03: 26: 16
