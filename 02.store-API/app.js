497e6e437ea136b75db9418a7878915bb5014c93
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-09-11 16:25:00 +0300

    Edited app.js

--- 02.store-API/app.js
+++ 02.store-API/app.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
-// async errors
+require('express-async-errors');
 
 const express = require("express");
 const app = express();
 
+const connectDB = require('./db/connect.js')
 const productsRouter = require('./routes/products')
 
 const notFoundMiddleware = require("./middleware/not-found.js");
@@ -18,7 +19,7 @@ app.get("/", (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/products">Products</a>');
 });
 
-app.use('/api/products', productsRouter)
+app.use('/api/v1/products', productsRouter);
 
 //  Proudcts route
 app.use(notFoundMiddleware);
